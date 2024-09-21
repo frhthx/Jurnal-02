469983373c665dfd186d246930e3ba132e380c22
frhthx <fathiaarahmah1006@gmail.com>
2024-09-21 11:44:24 +0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+class HelloWord {
+
+    public static void main(String[] args) {
+        System.out.println("Nama : Fathia Rahmah");
+        System.out.println("NIM : 607061400064");
+        System.out.println("Kelas : D3IF-48-01");
+    }
+}
